0021f7ecdbb753cc320313a7d2f4230dda875deb
Wixden <watchonlytutorial@gmail.com>
2023-05-21 20:20:26 +0600

    Delete Method Added

--- index.js
+++ index.js
@@ -87,6 +87,14 @@ async function run() {
       res.send(result);
     });
 
+    // DELETE METHOD
+    app.delete("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
