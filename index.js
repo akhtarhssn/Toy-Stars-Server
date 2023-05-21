bd78339dd5e36681192828ef6b3a1083aeecb2f0
Wixden <watchonlytutorial@gmail.com>
2023-05-21 18:25:39 +0600

    Vercel server deployment complete

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
     const toyCollection = client.db("toyDb").collection("toys");
     const categoryCollection = client.db("toyDb").collection("category");
